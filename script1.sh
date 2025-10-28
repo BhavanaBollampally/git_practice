005cff805677b651ff728f74d9decbe29c44439c
BhavanaBollampally <bhavanabollampally3@gmail.com>
2025-10-28 10:18:36 +0530

    Script1 added

---
+++ script1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+#Author: Bhavana
+#Script1:Build habit of writing & running scripts.
+
+#Print 3 lines — name, date, system type.
+echo " Name : Bhavana"
+Date=$(date +"%Y-%m-%d")
+echo "Date: [$Date]"
+echo "System type : Ubuntu latest"
+
