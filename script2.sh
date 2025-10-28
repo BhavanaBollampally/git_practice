f829db2104ccd3c6c7bdd659e77b3a4181eb021d
BhavanaBollampally <bhavanabollampally3@gmail.com>
2025-10-28 10:28:15 +0530

    Script2 added

---
+++ script2.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+#Author: Bhavana
+#Script2:Train variable recall & string insertion.
+
+#Store your name, current time, and course in variables.
+#Print them in one sentence using echo.
+#Print them again in separate lines.
+
+Name=Bhavana
+Time=$(date +"%H %M %S")
+Course="Shell Scripting"
+
+echo " $Name,$Time,$Course"
+echo "Name: $Name"
+echo "Time: $Time"
+echo "Course:$Course"
