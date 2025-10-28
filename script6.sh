d92cbca4ede89d77e2bc81a909e85bc46cb4984f
BhavanaBollampally <bhavanabollampally3@gmail.com>
2025-10-28 11:06:33 +0530

    Script6 added

---
+++ script6.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+#Author: Bhavana
+#Script 6 — Simple Loop-Train repetition reasoning.
+#Print numbers 1–5.
+#After each number, print “Iteration< n >.”
+
+for n in {1..5}
+do
+echo "Iteration $n"
+done
